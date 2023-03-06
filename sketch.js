8c3d06f8d210a903926e0cfed52ea46daa7c4d9e
Galaxia5 <119637290+Galaxia5@users.noreply.github.com>
2023-03-06 17:00:34 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var gato;
+
+function setup(){
+createCanvas(600,400);
+gato = createSprite(200,200,40,40);
+}
+
+function draw(){
+background("black");
+
+if(keyIsDown(UP_ARROW)){
+    gato.y = gato.y-5;
+}
+if(keyIsDown(DOWN_ARROW)){
+    gato.y = gato.y+5;
+}
+drawSprites();
+}
